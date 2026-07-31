0a5832c02cf753d04eb1a7887f92ac7b572fdf6c
Jayesh-Motwani <motwanij63@gmail.com>
2026-08-01 00:48:58 +0530

    first push

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,12 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+MY_PROJECT = os.environ.get("MY_PROJECT_NAME")
+API_KEY = os.environ.get("API_KEY")
+
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World AGAIN!"}
+    return {"Hello": "World AGAIN!", "project_name": MY_PROJECT}
